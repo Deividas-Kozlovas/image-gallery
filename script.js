440f69411600166a3cc6a828288bce0c40aa7354
Deividas Kozlovas <kozlovas.deividas@gmail.com>
2024-10-28 12:19:26 +0200

    implemented galery image mix and fixed that galery will show one list of imagees will not repeat

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
 import { imagesList } from "./imagesList.js";
 
 (() => {
-  const addImagesBtn = document.querySelector("#add_images"); // Use correct ID
+  const addImagesBtn = document.querySelector("#add_images");
 
-  // Add click event listener
   addImagesBtn.addEventListener("click", () => {
-    console.log("clicked");
-    addImages(); // Call the addImages function on button click
+    addImages(imagesList);
   });
 
-  // Define the addImages function
-  function addImages() {
+  const mixImagesBtn = document.querySelector("#mix_images");
+
+  mixImagesBtn.addEventListener("click", () => {
+    const shuffledImages = shuffleArray(imagesList);
+    addImages(shuffledImages);
+  });
+
+  function clearGallery() {
+    const gallery = document.querySelector(".gallery");
+    gallery.innerHTML = "";
+  }
+
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
+  function addImages(imagesList) {
+    clearGallery();
     const gallery = document.querySelector(".gallery");
     imagesList.forEach((img) => {
       const imageDiv = document.createElement("div");
@@ -18,11 +35,13 @@ import { imagesList } from "./imagesList.js";
 
       const imgElement = document.createElement("img");
       imgElement.classList.add("gallery__image");
-      imgElement.src = `img/${img}.jpg`; // Ensure correct path to the images
-      imgElement.alt = `Gallery image`; // Alt text for accessibility
+      imgElement.src = `img/${img}.jpg`;
+      imgElement.alt = `Gallery image`;
 
       imageDiv.appendChild(imgElement);
       gallery.appendChild(imageDiv);
     });
+
+    mixImagesBtn.style.display = "block";
   }
 })();
